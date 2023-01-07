66b1d2ebf1c494f095e5f05a9ce229e68413ce46
Milton N Santiago <MiltonNSantiago@gmail.com>
2023-01-06 19:14:46 -0600

    added gitignore

--- warhammertrivia/settings.py
+++ warhammertrivia/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from secret_settings import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9-!*)md+2g*iejl0opu*o@c#0#)va3*wbfz&!9a6=%_eh!9r2g'
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
